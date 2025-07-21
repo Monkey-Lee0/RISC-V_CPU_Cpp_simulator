efecc2cafea7435b8d833575d07c5e1b8d87d555
LJY <2562156161@qq.com>
2025-07-22 00:22:34 +0800

    Add decoder module and some debug supports.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
+#include"decoder.h"
+Decoder::instruction ins[5000];// instructions
+int cntIns=0;
+unsigned int reg[32];// registers
+unsigned int pc;// pc pointer
+unsigned int mem[100000];// memory
 int main()
 {
-    std::cout<<"Hello!"<<std::endl;
+    const auto ch=new char*[4];
+    for(int i=0;i<4;i++)
+        ch[i]=new char[30];
+    int kd=0;
+    while(scanf("%s",ch[kd])!=EOF)
+    {
+        if(ch[kd][0]=='@')
+            continue;
+        if(ch[kd][0]=='!')
+            break;
+        kd++;
+        if(kd==4)
+        {
+            ins[++cntIns]=Decoder::launchDecoder(ch);
+            if(ins[cntIns].op.empty())
+                cntIns--;
+            kd=0;
+        }
+    }
+    for(int i=1;i<=cntIns;i++)
+        std::cout<<ins[i]<<std::endl;
 
     return 0;
 }
